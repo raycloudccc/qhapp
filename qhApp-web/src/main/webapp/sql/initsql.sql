c0f711539d6845e048d632145275e1fee344f496
陈超超 <50830029@qq.com>
2017-04-27 16:23:50 +0800

    sql提交

--- qhApp-web/src/main/webapp/sql/initsql.sql
+++ qhApp-web/src/main/webapp/sql/initsql.sql
@@ -74,3 +74,43 @@ CREATE TABLE `subject` (
 -- ----------------------------
 INSERT INTO `subject` VALUES ('1', '钢琴');
 INSERT INTO `subject` VALUES ('2', '琵琶');
+
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher` (
+  `teacher_id` varchar(32) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `age` int(11) default NULL,
+  `gender` int(11) NOT NULL,
+  `tele` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `wxh` varchar(255) default NULL COMMENT '微信号',
+  `address` varchar(255) default NULL,
+  `sub_id` varchar(40) NOT NULL COMMENT '所属学科',
+  `status` int(11) NOT NULL,
+  `memo` varchar(255) default NULL,
+  `createtime` timestamp NOT NULL default '0000-00-00 00:00:00' on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`teacher_id`),
+  UNIQUE KEY `teacher_id` USING HASH (`teacher_id`),
+  KEY `name` USING HASH (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES ('eaf537ff934745f1ba4ad038ecc3d998', '幅度高达', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 13:19:41');
+INSERT INTO `teacher` VALUES ('f649201e572f48e986666ba5b386dde6', '1111111', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 13:40:43');
+INSERT INTO `teacher` VALUES ('abe7c3781ebf45a6b125c7e55a82f513', 'noname', '27', '1', '15921655030', '824160554@qq.com', 'aaaaa', '松江区', '2', '1', '啊啊啊啊啊啊啊啊啊', '2017-04-27 13:13:54');
+INSERT INTO `teacher` VALUES ('b27fe9b091944b929b1bc3f860bb1044', '陈二超', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 11:37:55');
+INSERT INTO `teacher` VALUES ('b8baa72901b3429e913b9be6e3b0fd83', '也浮动', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 14:16:34');
+INSERT INTO `teacher` VALUES ('98b43399c71a43edbf480de811dcdf02', '也', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 13:24:04');
+INSERT INTO `teacher` VALUES ('569601a25aec4e91b47da0dfcc0ef5a4', '也', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 13:24:04');
+INSERT INTO `teacher` VALUES ('eaa0869ca11e45bd8d21443f6a14a6c1', '也个', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 14:13:47');
+INSERT INTO `teacher` VALUES ('6115f4a3c9e44e3b891923f49d603f33', 'aa', '33', '1', '', '', '', '', '2', '1', '', '2017-04-27 15:41:54');
+INSERT INTO `teacher` VALUES ('f96743ee2684456e877bfc831e902edd', 'A', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 15:41:14');
+INSERT INTO `teacher` VALUES ('fe55b3a95c494a5ca8f2d8b9369a63ba', '通用语', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 13:26:30');
+INSERT INTO `teacher` VALUES ('f43a3ffb3106424c8ef293d3578b6fb6', 'ttt4', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 14:08:41');
+INSERT INTO `teacher` VALUES ('8cfec200294a4e60886f59423cc24cca', '轻微污染', null, '1', '', '', '', '', '1', '1', '', '2017-04-27 14:11:37');
+INSERT INTO `teacher` VALUES ('03c6c96dc03d4eb7858bb53a42dd4471', '大多数', null, '1', '', '', '', '', '2', '1', '', '2017-04-27 14:16:51');
